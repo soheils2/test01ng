455b38cebf736855d1b5a55300ddbc0ab6656484
soheils2 <soheilasami@gmail.com>
2023-08-08 20:58:19 +0330

    ✅ Register Feature Completed

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import {
   FormControl,
@@ -12,6 +12,17 @@ import { Router, RouterLink } from '@angular/router';
 import { LoginRequest } from '../../shared/login-request.model';
 import { AccountService } from '../../core/account.service';
 import { NotifyService } from 'src/app/shared/notify.service';
+import {
+  Register,
+  PRESET_RegisterRequest,
+} from 'src/app/shared/register.model';
+import { catchError, map, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+enum STTs {
+  Login = 0,
+  Register = 1,
+}
 
 @Component({
   selector: 'app-login',
@@ -25,11 +36,22 @@ export class LoginComponent implements OnInit {
   submitAttempted = false;
   message = '';
 
+  inUse: boolean = false;
+
+  @ViewChild('registerForm') registerForm: FormGroup;
+  @ViewChild('loginFrom') loginFrom: FormGroup;
+
+  sttTypes = STTs;
+  sttState = this.sttTypes.Login;
+  setSttState(_state: STTs) {
+    this.sttState = _state;
+  }
+
   ngOnInit() {
-    this.credentials = { username: '', password: '' };
+    this.credentials = { username: '', password: '', re_password: '' };
     if (this.accountService.isAuthenticated) this.router.navigate(['/']);
   }
-  credentials = { username: '', password: '' };
+  credentials = { username: '', password: '', re_password: '' };
 
   constructor(
     private notify: NotifyService,
@@ -39,7 +61,7 @@ export class LoginComponent implements OnInit {
 
   onLogin(_form: any) {
     this.submitAttempted = true;
-    // console.warn('_form:', _form);
+    console.warn('_form:', _form);
     if (_form.valid) {
       let loginVM: LoginRequest = {
         email: this.credentials.username,
@@ -75,20 +97,49 @@ export class LoginComponent implements OnInit {
       this.notify.warn('❗️ لطفا مقادیر وارد شده را بررسی نمایید');
     }
   }
+  onRegister(_form: any) {
+    this.submitAttempted = true;
+    // console.warn('_form:', _form);
+    if (_form.valid) {
+      let registerVM: Register = {
+        ...PRESET_RegisterRequest,
+        email: this.credentials.username,
+        password: this.credentials.password,
+        confirmPassword: this.credentials.re_password,
+      };
+      if (registerVM.password != registerVM.confirmPassword) {
+        this.notify.warn('رمز های وارد شده همخوانی ندارد');
+        return;
+      } else {
+        this.accountService.register(registerVM).subscribe({
+          complete: () => {
+            console.log('complete', this.loginFrom);
+            this.notify.success('ثبت نام با موفقیت انجام شد لطفا وارد شوید');
+            this.sttState = this.sttTypes.Login;
+          },
+          error: (error) => {
+            let _sttCode = error.status;
+            console.log('error 2!', error);
+            if (_sttCode == 409) {
+              this.registerForm.controls['email'].setErrors({ inUse: true });
+            } else {
+              this.notify.warn('فرمت اطلاعات وارد شده ناقص یا نادرست است');
+            }
+          },
+          next: (response) => {
+            // console.log('next', response);
+          },
+        });
+      }
+    } //
+    else {
+      this.notify.warn('❗️ لطفا مقادیر وارد شده را بررسی نمایید');
+    }
+  }
 
   log(x: any) {
     console.log(x);
   }
-  cop = '';
-  loginForm = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required]),
-    pass: new FormControl('', [Validators.email, Validators.required]),
-  });
-
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
 
   loginSucces() {
     this.notify.success('ورود موفق ِدرحال انتقال به داشبورد', 3000);
